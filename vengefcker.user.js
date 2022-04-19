8ab15b324f27eabb07cba2734dc0de300f2847cb
AnonHexo <61375258+AnonHexo@users.noreply.github.com>
2022-04-19 11:50:45 +0200

    v1

---
+++ vengefcker.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         VengeFcker Public
+// @version      1
+// @description  Venge.io script
+// @author       AnonHexo#3411
+// @match        *://*venge.io/*
+// @require      https://raw.githubusercontent.com/AnonHexo/VengeFcker/main/src/vengefcker.js
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
